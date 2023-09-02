599740e2a7bdd3b8812cb19681e999ebe497dea3
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-09-03 00:02:07 +0300

    ViewBunding

--- app/src/main/java/com/example/landmarkbook/DetailsActivity.java
+++ app/src/main/java/com/example/landmarkbook/DetailsActivity.java
@@ -3,12 +3,20 @@ package com.example.landmarkbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.landmarkbook.databinding.ActivityDetailsBinding;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater()); //xml ile kodu bağlama
+        View view = binding.getRoot(); //xml yerine bagladigimiz yer cikiyor
+        setContentView(view);
+
     }
 }
